2e53bd821e60d3a17da15884ff72edbc97e48c6c
shreyansh4155 <shreyanshg245@gmail.com>
2025-03-04 12:27:14 +0530

    [Shreyansh]Add.Function to get working hours

--- src/employeewages.js
+++ src/employeewages.js
@@ -1,19 +1,30 @@
-const IS_PRESENT = Math.random() < 0.5; // 50% probability
-console.log(IS_PRESENT ? "Employee is Present" : "Employee is Absent");
-
 const FULL_TIME_HOURS = 8,
   PART_TIME_HOURS = 4,
   WAGE_PER_HOUR = 20;
-let empType = Math.floor(Math.random() * 3);
-let workHours =
-  empType === 1 ? PART_TIME_HOURS : empType === 2 ? FULL_TIME_HOURS : 0;
 
-if (IS_PRESENT) {
-  console.log(
-    `Employee worked ${workHours} hours and earned $${
-      workHours * WAGE_PER_HOUR
-    }`
-  );
-} else {
-  console.log("No wages, employee was absent.");
+function getWorkHours(empType) {
+  return empType === 1 ? PART_TIME_HOURS : empType === 2 ? FULL_TIME_HOURS : 0;
 }
+
+function calculateWage() {
+  const IS_PRESENT = Math.random() < 0.5; // 50% probability
+  let empType = Math.floor(Math.random() * 3);
+  let workHours = IS_PRESENT ? getWorkHours(empType) : 0;
+
+  if (IS_PRESENT) {
+    console.log("Employee is Present");
+    console.log(
+      `Employee worked ${workHours} hours and earned $${
+        workHours * WAGE_PER_HOUR
+      }`
+    );
+  } else {
+    console.log("Employee is Absent");
+    console.log("No wages, employee was absent.");
+  }
+
+  console.log(`Employee worked ${workHours} hours`);
+}
+
+// Call the function to calculate wage
+calculateWage();
